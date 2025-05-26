4e96f88d90f2d792c19a7eac162dec4b524cc20e
HYDRAoo7 <pandyavedant6565@gmail.com>
2025-05-26 17:42:11 +0530

    canara

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,20 @@ const WindowSize = struct {
     pub const height: c_int = 600;
 };
 
+var cameraPos = zm.f32x4(0, 0, 3, 0);
+const cameraFront = zm.f32x4(0, 0, -1, 0);
+const cameraUp = zm.f32x4(0, 1, 0, 0);
+
+const firstMouse = true;
+var yaw: f32 = -90.0;
+var pitch: f32 = 0.0;
+var lastX: f32 = 800.0 / 2.0;
+var lastY: f32 = 600.0 / 2.0;
+var fov: f32 = 45.0;
+
+var deltaTime: f32 = 0;
+var lastFrame: f32 = 0;
+
 var procs: gl.ProcTable = undefined;
 const rad_conversion = std.math.pi / 180.0;
 
@@ -63,47 +77,47 @@ pub fn main() !void {
 
     const vertices = [_]f32{
         // positions          // texture coords
-        -0.5, -0.5, -0.5, 0.0,  0.0,
-        0.5,  -0.5, -0.5, 1.0,  0.0,
-        0.5,  0.5,  -0.5, 1.0,  1.0,
-        0.5,  0.5,  -0.5, 1.0,  1.0,
-        -0.5, 0.5,  -0.5, 0.0,  1.0,
-        -0.5, -0.5, -0.5, 0.0,  0.0,
+        -0.5, 0.5,  -0.5, 0.25, 0.6666,
+        0.5,  0.5,  -0.5, 0.50, 0.6666,
+        0.5,  0.5,  0.5,  0.50, 1.0,
+        0.5,  0.5,  0.5,  0.50, 1.0,
+        -0.5, 0.5,  0.5,  0.25, 1.0,
+        -0.5, 0.5,  -0.5, 0.25, 0.6666,
 
-        -0.5, -0.5, 0.5,  0.0,  0.0,
-        0.5,  -0.5, 0.5,  1.0,  0.0,
-        0.5,  0.5,  0.5,  1.0,  1.0,
-        0.5,  0.5,  0.5,  1.0,  1.0,
-        -0.5, 0.5,  0.5,  0.0,  1.0,
-        -0.5, -0.5, 0.5,  0.0,  0.0,
+        -0.5, -0.5, 0.5,  0.0,  0.3333,
+        0.5,  -0.5, 0.5,  0.25, 0.3333,
+        0.5,  0.5,  0.5,  0.25, 0.6666,
+        0.5,  0.5,  0.5,  0.25, 0.6666,
+        -0.5, 0.5,  0.5,  0.0,  0.6666,
+        -0.5, -0.5, 0.5,  0.0,  0.3333,
 
-        -0.5, 0.5,  0.5,  1.0,  0.0,
-        -0.5, 0.5,  -0.5, 1.0,  1.0,
-        -0.5, -0.5, -0.5, 0.0,  1.0,
-        -0.5, -0.5, -0.5, 0.0,  1.0,
-        -0.5, -0.5, 0.5,  0.0,  0.0,
-        -0.5, 0.5,  0.5,  1.0,  0.0,
+        -0.5, -0.5, -0.5, 0.25, 0.3333,
+        0.5,  -0.5, -0.5, 0.50, 0.3333,
+        0.5,  0.5,  -0.5, 0.50, 0.6666,
+        0.5,  0.5,  -0.5, 0.50, 0.6666,
+        -0.5, 0.5,  -0.5, 0.25, 0.6666,
+        -0.5, -0.5, -0.5, 0.25, 0.3333,
 
-        0.5,  0.5,  0.5,  1.0,  0.0,
-        0.5,  0.5,  -0.5, 1.0,  1.0,
-        0.5,  -0.5, -0.5, 0.0,  1.0,
-        0.5,  -0.5, -0.5, 0.0,  1.0,
-        0.5,  -0.5, 0.5,  0.0,  0.0,
-        0.5,  0.5,  0.5,  1.0,  0.0,
-
-        -0.5, -0.5, -0.5, 0.0,  1.0,
-        0.5,  -0.5, -0.5, 1.0,  1.0,
-        0.5,  -0.5, 0.5,  1.0,  0.0,
-        0.5,  -0.5, 0.5,  1.0,  0.0,
-        -0.5, -0.5, 0.5,  0.0,  0.0,
-        -0.5, -0.5, -0.5, 0.0,  1.0,
-
-        -0.5, 0.5,  -0.5, 0.25, 1.0,
-        0.5,  0.5,  -0.5, 1.0,  1.0,
-        0.5,  0.5,  0.5,  1.0,  0.3333,
-        0.5,  0.5,  0.5,  1.0,  0.3333,
         -0.5, 0.5,  0.5,  0.25, 0.3333,
-        -0.5, 0.5,  -0.5, 0.25, 1.0,
+        -0.5, 0.5,  -0.5, 0.25, 0.6666,
+        -0.5, -0.5, -0.5, 0.0,  0.6666,
+        -0.5, -0.5, -0.5, 0.0,  0.6666,
+        -0.5, -0.5, 0.5,  0.0,  0.3333,
+        -0.5, 0.5,  0.5,  0.25, 0.3333,
+
+        0.5,  0.5,  0.5,  1.0,  0.3333,
+        0.5,  0.5,  -0.5, 1.0,  0.6666,
+        0.5,  -0.5, -0.5, 0.75, 0.6666,
+        0.5,  -0.5, -0.5, 0.75, 0.6666,
+        0.5,  -0.5, 0.5,  0.75, 0.3333,
+        0.5,  0.5,  0.5,  1.0,  0.3333,
+
+        -0.5, -0.5, -0.5, 0.25, 0.3333,
+        0.5,  -0.5, -0.5, 0.50, 0.3333,
+        0.5,  -0.5, 0.5,  0.50, 0.0,
+        0.5,  -0.5, 0.5,  0.50, 0.0,
+        -0.5, -0.5, 0.5,  0.25, 0.0,
+        -0.5, -0.5, -0.5, 0.25, 0.3333,
     };
 
     const indices = [_][3]f32{
@@ -150,7 +164,7 @@ pub fn main() !void {
 
     zstbi.init(allocator);
     defer zstbi.deinit();
-    zstbi.setFlipVerticallyOnLoad(true);
+    // zstbi.setFlipVerticallyOnLoad(true);
 
     // gl.BindBuffer(gl.ARRAY_BUFFER, 0);
     // gl.BindVertexArray(0);
@@ -174,49 +188,49 @@ pub fn main() !void {
     gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(image.width), @intCast(image.height), 0, gl.RGB, gl.UNSIGNED_BYTE, @ptrCast(image.data));
     gl.GenerateMipmap(gl.TEXTURE_2D);
 
-    var texture2: c_uint = undefined;
-    gl.GenTextures(1, (&texture2)[0..1]);
-    gl.ActiveTexture(gl.TEXTURE1);
-    gl.BindTexture(gl.TEXTURE_2D, texture2);
+    // var texture2: c_uint = undefined;
+    // gl.GenTextures(1, (&texture2)[0..1]);
+    // gl.ActiveTexture(gl.TEXTURE1);
+    // gl.BindTexture(gl.TEXTURE_2D, texture2);
+    //
+    // gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    // gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    //
+    // gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    // gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-    const image_path2 = try pathToContent(arena, "texture/awesomeface.png");
-    var image2 = try zstbi.Image.loadFromFile(image_path2, 0);
-    defer image2.deinit();
-
-    gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(image2.width), @intCast(image2.height), 0, gl.RGBA, gl.UNSIGNED_BYTE, @ptrCast(image2.data));
+    // const image_path2 = try pathToContent(arena, "texture/awesomeface.png");
+    // var image2 = try zstbi.Image.loadFromFile(image_path2, 0);
+    // defer image2.deinit();
+    //
+    // gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB, @intCast(image2.width), @intCast(image2.height), 0, gl.RGBA, gl.UNSIGNED_BYTE, @ptrCast(image2.data));
 
     gl.Enable(gl.DEPTH_TEST);
 
     ourShader.use();
     ourShader.setInt("texture1", 0);
-    ourShader.setInt("texture2", 1);
+    // ourShader.setInt("texture2", 1);
 
     var projection: [16]f32 = undefined;
     var view: [16]f32 = undefined;
     var model: [16]f32 = undefined;
 
-    const trans2 = zm.translation(0, 0, -3);
-    zm.storeMat(&view, trans2);
-    ourShader.setMat4f("view", view);
-
     while (!glfw.windowShouldClose(window)) {
+        const currentFrame = @as(f32, @floatCast(glfw.getTime()));
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         processInput(window);
 
+        gl.ClearColor(0.2, 0.3, 0.3, 1);
         gl.Clear(gl.DEPTH_BUFFER_BIT);
         gl.Clear(gl.COLOR_BUFFER_BIT);
-        gl.ClearColor(0.2, 0.3, 0.3, 1);
 
         gl.ActiveTexture(gl.TEXTURE0);
         gl.BindTexture(gl.TEXTURE_2D, texture);
 
-        gl.ActiveTexture(gl.TEXTURE1);
-        gl.BindTexture(gl.TEXTURE_2D, texture2);
+        // gl.ActiveTexture(gl.TEXTURE1);
+        // gl.BindTexture(gl.TEXTURE_2D, texture2);
 
         ourShader.use();
 
@@ -224,15 +238,21 @@ pub fn main() !void {
         // gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
 
         const window_size = window.getSize();
-        const fov = @as(f32, @floatFromInt(window_size[0])) / @as(f32, @floatFromInt(window_size[1]));
-        const prt = zm.perspectiveFovRhGl(45.0 * rad_conversion, fov, 0.1, 100.0);
+        const aspect = @as(f32, @floatFromInt(window_size[0])) / @as(f32, @floatFromInt(window_size[1]));
+        const prt = zm.perspectiveFovRhGl(fov * rad_conversion, aspect, 0.1, 100.0);
         zm.storeMat(&projection, prt);
 
         ourShader.setMat4f("projection", projection);
 
+        const trans2 = zm.lookAtRh(cameraPos, cameraPos + cameraFront, cameraUp);
+        zm.storeMat(&view, trans2);
+        ourShader.setMat4f("view", view);
+
         for (indices, 0..) |indice, i| {
-            const angle = (((@mod(@as(f32, @floatFromInt(i + 1)), 2.0)) * 2.0) - 1.0);
-            const rotate = zm.matFromAxisAngle(zm.f32x4(1.0, 0.3, 0.5, 1.0), @as(f32, @floatCast(glfw.getTime())) * 55.0 * angle * rad_conversion);
+            // const angle = (((@mod(@as(f32, @floatFromInt(i + 1)), 2.0)) * 2.0) - 1.0);
+            _ = (((@mod(@as(f32, @floatFromInt(i + 1)), 2.0)) * 2.0) - 1.0);
+            // const rotate = zm.matFromAxisAngle(zm.f32x4(1.0, 0.3, 0.5, 1.0), @as(f32, @floatCast(glfw.getTime())) * 55.0 * angle * rad_conversion);
+            const rotate = zm.rotationZ(0);
             const cube = zm.translation(indice[0], indice[1], indice[2]);
             const trans = zm.mul(rotate, cube);
             zm.storeMat(&model, trans);
@@ -240,7 +260,7 @@ pub fn main() !void {
             gl.DrawArrays(gl.TRIANGLES, 0, 36);
         }
 
-        gl.DrawArrays(gl.TRIANGLES, 0, 3);
+        // gl.DrawArrays(gl.TRIANGLES, 0, 3);
 
         window.swapBuffers();
         glfw.pollEvents();
@@ -256,6 +276,16 @@ fn processInput(window: *glfw.Window) void {
     if (glfw.getKey(window, glfw.Key.escape) == glfw.Action.press) {
         glfw.setWindowShouldClose(window, true);
     }
+
+    const cameraSpeed = zm.f32x4s(2.5 * deltaTime);
+    if (glfw.getKey(window, glfw.Key.w) == glfw.Action.press)
+        cameraPos += cameraFront * cameraSpeed;
+    if (glfw.getKey(window, glfw.Key.s) == glfw.Action.press)
+        cameraPos -= cameraFront * cameraSpeed;
+    if (glfw.getKey(window, glfw.Key.a) == glfw.Action.press)
+        cameraPos -= zm.normalize4(zm.cross3(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfw.getKey(window, glfw.Key.d) == glfw.Action.press)
+        cameraPos += zm.normalize4(zm.cross3(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 pub fn pathToContent(arena: std.mem.Allocator, resource_relative_path: []const u8) ![:0]const u8 {
